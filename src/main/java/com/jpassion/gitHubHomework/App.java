79c7443d68ba794cd84f64420918b2aa035b1221
Norman Gies <gies.norman@googlemail.com>
2015-02-01 16:02:59 +0100

    initial commit

---
+++ src/main/java/com/jpassion/gitHubHomework/App.java
@@ -0,0 +1,13 @@
+package com.jpassion.gitHubHomework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
